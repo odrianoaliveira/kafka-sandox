07e5e67a99a89fa254b86394e1e6a448171ba9d3
Adriano Oliveira <5173823+odrianoaliveira@users.noreply.github.com>
2021-08-24 10:11:40 +0200

    creating gitignore

---
+++ src/main/java/tec/adriano/tutorial1/ProducerDemo.java
@@ -0,0 +1,32 @@
+package tec.adriano.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
+public class ProducerDemo {
+//    private static final Logger logger = LoggerFactory.getLogger(ProducerDemo.class);
+    public static final String BOOTSTRAP_SERVERS = "localhost:9092";
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+
+        properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("tech.adriano.kafka.test", "Hello Kafka from Java!");
+
+        producer.send(record);
+        producer.flush();
+        producer.close();
+    }
+}
